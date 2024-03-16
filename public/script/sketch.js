09ad4160db0f94986bec17f861d1c2127d3c4bcd
DESKTOP-49UIQP7\morik <morikawa6600@gmail.com>
2024-03-16 21:41:18 +0900

    描画処理のテスト

---
+++ public/script/sketch.js
@@ -0,0 +1,11 @@
+// sketch.js
+function setup() {
+  createCanvas(640, 480);
+  fill(0);
+
+  ellipse(50, 50, 100, 100);
+
+  rect(100, 100, 100, 100);
+
+  triangle(250, 200, 200, 300, 300, 300);
+}
